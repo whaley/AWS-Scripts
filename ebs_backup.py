9021f603ecc243354a9871673a5d4837e8e91a92
whaley <jason@nodeable.com>
2012-05-17 11:37:19 -0700

    Fixed compiler errors and pep8 in ebs_backup.py
    Many more pep8 errors to correct. Now compiles properly.

--- ebs_backup.py
+++ ebs_backup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 import boto
-import sys
 from optparse import OptionParser
 from datetime import datetime
 
+
 def get_arg_parser():
     parser = OptionParser()
-    parser.add_option("-n","--name", dest="name_tag", 
+    parser.add_option("-n","--name", dest="name_tag",
         help="'Name' tag to be applied to snapshot.  Does not override 'Name'" +
         " from volume.  Must be supplied if 'Name' tag is not set on volume" +
         " being snapshotted")
@@ -15,8 +15,8 @@ def get_arg_parser():
 
 def generate_description(base_name=""):
     now = datetime.now()
-    return base_name + "-" + str(now.year) + "." + str(now.month).zfill(2) + 
-        "." + str(now.day).zfill(2) + "." + str(now.hour).zfill(2) + 
+    return base_name + "-" + str(now.year) + "." + str(now.month).zfill(2) + \
+        "." + str(now.day).zfill(2) + "." + str(now.hour).zfill(2) + \
         str(now.minute).zfill(2)
 
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     (options,args) = get_arg_parser().parse_args()
     volume_id = args[0] if len(args) > 0 else None
     if volume_id is None or "vol" not in volume_id.strip():
-        raise Exception("Invalid argument for volume_id.  ' +
+        raise Exception("Invalid argument for volume_id.  " + \
             "Please provide an argument of the volume_id to be snapshotted.")
 
     ec2 = boto.connect_ec2()
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         lambda boto_volume: volume_id == boto_volume.id, ec2.get_all_volumes())
     volume = volumes[0] if len(volumes) > 0 else None
     if volume is None: 
-        vraise Exception(volume_id + " not found in volumes for account")
+        raise Exception(volume_id + " not found in volumes for account")
 
     #Make sure a 'Name' tag gets created for the snapshot 
     name_tag = None
